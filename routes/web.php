7daf5539def9a98bbf442a4d530ab50b3adaee9d
abienias97 <wonszarek@gmail.com>
2022-12-28 13:34:48 +0100

    Making views and viewing things from database

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Currency;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $allListings = Currency::all();
+    if($allListings->count()>0){
+        return view('getCurrency', [
+            'exchangeRates' => $allListings
+        ]);
+    } else {
+        return view('noCurrency');
+    }
+
 });
